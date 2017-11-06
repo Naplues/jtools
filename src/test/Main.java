1db670baf009cb76f4f3c11ff10ffa833d6a2e79
naplues <naplues@163.com>
2017-11-07 00:18:00 +0800

    写入文件BufferedWriter Signed-off-by: naplues <naplues@163.com>

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,11 +1,14 @@
 package test;
 
+import java.util.List;
+
 import file.FileHandle;
 
 public class Main {
 	public static void main(String[] args) {
-		String text = FileHandle.readFileToString("resource/test.txt");
-		System.out.println(text);
-		
+		String filePath = "resource/test.txt";
+		List<String> strings = FileHandle.readFileToLines(filePath);
+		filePath = "resource/a.txt";
+		FileHandle.writeLinesToFile(filePath, strings,true);
 	}
 }
